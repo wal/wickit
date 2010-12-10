7d5b5bdd54452b1c61891cb91d3c1427e0f6d5aa
Wal McConnell <walter.mcconnell@gmail.com>
2010-12-10 09:13:40 +0000

    simple context support

--- examples/example_test.js
+++ examples/example_test.js
@@ -1,11 +1,15 @@
 var Wick = require('../lib/wickit').Wick;
 var assert = require('assert');
 
-Wick.it("test should pass", function() {
-  assert.ok(true);
-});
+Wick.context("simple tests", function() {
+  // a context is optional, it is purely organisational for output purposes
 
-Wick.it("test should fail", function() {
-  assert.ok(false);
-});
 
+  Wick.it("test should pass", function() {
+    assert.ok(true);
+  });
+
+  Wick.it("test should fail", function() {
+    assert.ok(false);
+  });
+});
